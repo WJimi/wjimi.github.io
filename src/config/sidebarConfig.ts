3a56bf9a421d962f97637a3dab8e9d78a22f9dea
wjimi <3187243713@qq.com>
2026-08-14 19:20:30 +0800

    动态与CS2指令集

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -83,7 +83,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：最新动态组件
 			type: "dynamic",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "top",
 			// 是否在文章详情页显示
